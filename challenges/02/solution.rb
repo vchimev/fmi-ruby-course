267e1a0faebaed7d4cf3040794ff8af5ffd018e7
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-10-29 10:58:27 +0300

    Update challenge 02 solution

--- challenges/02/solution.rb
+++ challenges/02/solution.rb
@@ -1,9 +1,9 @@
 def ordinalize(number)
-  custom = { '1' => 'st', '2' => 'nd', '3' => 'rd' }
-  digits = number.to_s.chars
-  if custom.key?(digits[-1]) && digits[-2] != '1'
-    digits.join + custom[digits[-1]]
+  suffix = { '1' => 'st', '2' => 'nd', '3' => 'rd' }
+  digits = number.to_s
+  if suffix.key?(digits[-1]) && digits[-2] != '1'
+    digits + suffix[digits[-1]]
   else
-    digits.join + 'th'
+    digits + 'th'
   end
 end
