8712dc41c45bd121b902b6fdc32d43acee5c2a2a
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-10-29 11:28:38 +0300

    Add challenge 03 tests

---
+++ challenges/03/sample_spec.rb
@@ -0,0 +1,9 @@
+describe '#fibonacci_like?' do
+  it { expect(fibonacci_like?([0, 1, 1, 2, 3, 5, 8, 13])).to be true }
+  it { expect(fibonacci_like?([2, 6, 8, 14, 22, 36])).to be true }
+  it { expect(fibonacci_like?([0, 1, 1, 2, 3, 8])).to be false }
+  it { expect(fibonacci_like?([2, -1, 1, 0, 1, 1, 2, 3])).to be true }
+  it { expect(fibonacci_like?([0.1, 0.1, 0.2, 0.3, 0.5, 0.8, 1.3])).to be true }
+  it { expect(fibonacci_like?([0, -0.1, -0.1, -0.2, -0.3, -0.5])).to be true }
+  it { expect(fibonacci_like?([0, -1, -1, -2, -3, -8])).to be false }
+end
