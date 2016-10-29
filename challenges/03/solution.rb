0bce5f63cee748b25a987d805de55b254ecb86f3
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-10-29 21:49:45 +0300

    Update challenge 03 solution

--- challenges/03/solution.rb
+++ challenges/03/solution.rb
@@ -1,11 +1,3 @@
 def fibonacci_like?(array)
-  fib_like = []
-  array.each_cons(3) do |subarray|
-    if subarray[0] + subarray[1] == subarray[2]
-      fib_like.push true
-    else
-      fib_like.push false
-    end
-  end
-  fib_like.include?(false) ? false : true
+  array.each_cons(3).all? { |a, b, c| a + b == c }
 end
