089967625445b7a5ce0fbe3fc142533f96dbd324
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-10-29 22:41:17 +0300

    Fix challenge 01 rubocop offenses

--- challenges/01/sample_spec.rb
+++ challenges/01/sample_spec.rb
@@ -1,8 +1,8 @@
 describe '#common_digits_count' do
   it { expect(common_digits_count(567, 678)).to eq(2) }
-  it { expect(common_digits_count(123444, 3445)).to eq(2) }
+  it { expect(common_digits_count(123_444, 3445)).to eq(2) }
   it { expect(common_digits_count(456, -2358)).to eq(1) }
-  it { expect(common_digits_count(12345, 67890)).to eq(0) }
+  it { expect(common_digits_count(12_345, 67_890)).to eq(0) }
   it { expect(common_digits_count(-456, -358)).to eq(1) }
   it { expect(common_digits_count(111, 111)).to eq(1) }
   it { expect(common_digits_count(111, -111)).to eq(1) }
