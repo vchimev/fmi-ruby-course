d805810d51aa72af6cda3023196e2dafce7cd3b3
Vasil Chimev <Vasil.Chimev@telerik.com>
2016-10-29 22:50:08 +0300

    Fix challenge 01 rubocop offenses

--- challenges/01/solution.rb
+++ challenges/01/solution.rb
@@ -1,7 +1,7 @@
 def common_digits_count(i, j)
   r = 0
-  f = i.to_s.gsub('-','').chars.uniq
-  s = j.to_s.gsub('-','').chars.uniq
-  f.each { |k| (s.include? k) == true ? r+=1 : r }
+  f = i.to_s.delete('-').chars.uniq
+  s = j.to_s.delete('-').chars.uniq
+  f.each { |k| (s.include? k) == true ? r += 1 : r }
   r
 end
